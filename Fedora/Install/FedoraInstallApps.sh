ee7ecc2b7c6f7eb4bf77c73053dffc50ac8cb376
Kevin Day <kevin.ch.day@gmail.com>
2021-06-20 13:51:12 -0500

    Initial Commit

---
+++ Fedora/Install/FedoraInstallApps.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# update system
+sudo yum update -y
+sudo yum upgrade -y
+
+# IDE
+sudo apt install geany -y
+
+# Programming
+sudo yum install git -y
+sudo yum install g++ -y
+sudo yum install ruby -y
+sudo yum install rails -y
+sudo yum install python -y
+sudo yum install php -y
+sudo yum install nodejs -y
+
+# web server
+sudo yum install apache2 -y
+
+# database
+sudo yum install mysql-server -y
+
+# network tools
+sudo yum install nmap -y
+sudo yum install wireshark -y
+
+# desktop enviroment
+sudo yum install cinnamon -y
+
---
+++ Ubuntu/Install/UbuntuInstallApps.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# update system
+sudo apt update -y
+sudo apt upgrade -y
+
+# IDE
+sudo apt install geany -y
+
+# Programming
+sudo apt install git -y
+sudo apt install g++ -y
+sudo apt install ruby -y
+sudo apt install rails -y
+sudo apt install python -y
+sudo apt install php -y
+sudo apt install nodejs -y
+
+# web server
+sudo apt install apache2 -y
+
+# database
+sudo apt install mysql-server -y
+
+# network tools
+sudo apt install nmap -y
+sudo apt install wireshark -y
+
+# desktop enviroment
+sudo apt install cinnamon -y
